937f3786745610fdeae107ffb8d28682d1e098fe
Khaled <khaledmourad23345@gmail.com>
2022-02-04 19:01:47 +0200

    turn debug to false

--- deltapestco/settings.py
+++ deltapestco/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '3xi4q=+2bo$8+_ehux6v2@q&*2lrhvny-!2r@+0jdw3=sg++#t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
