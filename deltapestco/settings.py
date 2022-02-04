d0bd0922e77ed747b548926a30666e2032685b05
Khaled <khaledmourad23345@gmail.com>
2022-02-04 18:39:50 +0200

    adding main app

--- deltapestco/settings.py
+++ deltapestco/settings.py
@@ -120,3 +120,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com' # gmail
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'khaledmourad23345@gmail.com'
+EMAIL_HOST_PASSWORD = '123698745@Khm'
